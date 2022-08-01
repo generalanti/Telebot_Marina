c15576451149361cca0267c1f2a99db9361ee06e
generalanti <generalanti@gmail.com>
2022-08-01 22:41:18 +0300

    aiogram_dialog. callback button

--- bot_main.py
+++ bot_main.py
@@ -1,12 +1,11 @@
 from aiogram import Bot, Dispatcher, executor
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher.filters.state import StatesGroup, State
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
 
 from aiogram_dialog import Window, Dialog, DialogRegistry, DialogManager, StartMode
 from aiogram_dialog.widgets.kbd import Button
-from aiogram_dialog.widgets.text import Multi, Const, Format, Case
-from typing import Dict
+from aiogram_dialog.widgets.text import Const
 
 storage = MemoryStorage()
 bot = Bot(token='5342176536:AAEknzzBVOeMu31IaPUogLs-98cpDFnPjYA')
@@ -17,44 +16,21 @@ registry = DialogRegistry(dp)
 class MySG(StatesGroup):
     main = State()
 
+async def go_clicked(c: CallbackQuery, button: Button, manager: DialogManager):
+    await c.message.answer("Going on!")
 
-async def get_data(**kwargs):
-    return {"color": "red", "number": 42}
-
-
-text = Multi(
-    Const("Hello!"),
-    Const("And goodbye!"),
-    sep=" ",
-)
-
-text2 = Case(
-    {
-        "red": Const("Square"),
-        "green": Const("Unicorn"),
-        "blue": Const("Moon"),
-    },
-    selector="color",
+go_btn = Button(
+    Const("Go"),
+    id="go",  # id is used to detect which button is clicked
+    on_click=go_clicked,
 )
 
 
-def parity_selector(data: Dict, case: Case, manager: DialogManager):
-    return data["number"] % 2
-
-
-text3 = Case(
-    {
-        0: Format("{number} is even!"),
-        1: Const("It is Odd"),
-    },
-    selector=parity_selector,
-)
 
 main_window = Window(
-    text, text2, text3,
-    Button(Const("Useless button"), id="nothing"),
+    Const("Hello, unknown person"),
+    go_btn,
     state=MySG.main,
-    getter=get_data,
 )
 
 dialog = Dialog(main_window)
